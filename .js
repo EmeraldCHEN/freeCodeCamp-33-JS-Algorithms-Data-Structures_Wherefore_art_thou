3b684b1c7b1d249ee7021e7adbbe3d17126df9af
EmeraldNZ <ahyuancy@gmail.com>
2018-12-26 17:35:18 +1300

    Update .js

--- .js
+++ .js
@@ -1,5 +1,4 @@
 function whatIsInAName(collection, source) {
-
     let sourceKeys = Object.keys(source);
     return collection.filter(function(obj){
         for(let i = 0; i < sourceKeys.length; i++){
@@ -10,5 +9,13 @@ function whatIsInAName(collection, source) {
         return true;
     })
 }
+const result = whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }); 
+// Return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]
 
-const result = whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }); //return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]
+
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou/
+
+
